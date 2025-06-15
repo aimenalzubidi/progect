e88e1d94ed11fa9b4c818565cacb3d1b4dd6db3e
unknown <aimenalzubidi@gmail.com>
2025-06-15 12:08:09 +0300

    first commit

---
+++ lib/pages/login.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login '),
+      ),
+      body: Container(
+        color: Colors.white,
+        child: Column(
+          children: [
+            Container(
+              margin:
+                  const EdgeInsets.symmetric(horizontal: 110, vertical: 100),
+              child: Image.asset("asset/images/download (4).jpg"),
+            ),
+            Container(
+              margin: const EdgeInsets.all(10),
+              decoration: const BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.all(Radius.circular(25))),
+              child: TextFormField(
+                keyboardType: TextInputType.name,
+                decoration: const InputDecoration(
+                    hintText: "Enter Your Name", border: InputBorder.none),
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Container(
+              margin: const EdgeInsets.all(10),
+              decoration: const BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.all(Radius.circular(25))),
+              child: TextFormField(
+                keyboardType: TextInputType.name,
+                decoration: const InputDecoration(
+                    hintText: "   Enter Passwrd", border: InputBorder.none),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
